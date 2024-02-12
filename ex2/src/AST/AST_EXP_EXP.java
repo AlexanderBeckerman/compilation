c73f6417cb85b606d52cc394d2b8707c53f64cdc
solaimani <solaimani@mail.tau.ac.il>
2024-02-13 00:13:24 +0200

    daniels classes

---
+++ ex2/src/AST/AST_EXP_EXP.java
@@ -0,0 +1,50 @@
+package AST;
+
+public class AST_EXP_EXP extends AST_EXP
+{
+	public AST_EXP exp;
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_EXP_EXP(AST_EXP exp)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.format("====================== exp -> LPAREN exp RPAREN\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.exp = exp;
+	}
+
+	/************************************************/
+	/* The printing message for the node */
+	/************************************************/
+	public void PrintMe()
+	{
+		/*******************************/
+		/* AST NODE TYPE = AST EXP EXP */
+		/*******************************/
+		System.out.format("AST NODE EXP EXP\n");
+        
+        // we need to print the appropriate field as well beacuse its an object (non terminal)
+        this.exp.PrintMe();
+
+		/*********************************/
+		/* Print to AST GRAPHIZ DOT file */
+		/*********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("EXP\nEXP\n"));
+
+        AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,exp.SerialNumber);
+	}
+}
