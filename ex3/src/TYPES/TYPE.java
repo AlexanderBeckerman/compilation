79abd9c3018916b69fc2c7646b75c1f4153de169
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-03-02 17:42:57 +0200

    Added type checking.

--- ex3/src/TYPES/TYPE.java
+++ ex3/src/TYPES/TYPE.java
@@ -16,4 +16,30 @@ public abstract class TYPE
 	/* isArray() */
 	/*************/
 	public boolean isArray(){ return false;}
+
+	/**
+	 * Checks if an object of a specific type is an instance of another type.
+	 *
+	 * @param instance_type The type of the object to be checked.
+	 * @param super_type    The type to check against as a potential superclass.
+	 * @return True if the object is an instance of the specified type or inherits from it; otherwise, false.
+	 */
+	public boolean isInstanceOf(TYPE instance_type, TYPE super_type) {
+		if (instance_type instanceof TYPE_CLASS) {
+			TYPE_CLASS temp = (TYPE_CLASS) instance_type;
+
+			// Traverse the class hierarchy using the father reference
+			while (temp != null) {
+				if (super_type == instance_type) {
+					return true;
+				}
+				temp = temp.father;
+			}
+			// If the super_type is not found in the class hierarchy, return false
+			return false;
+		}
+
+		// If the instance_type is not a class type, compare it directly with the super_type
+		return instance_type == super_type;
+	}
 }
