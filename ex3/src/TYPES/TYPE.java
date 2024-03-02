59f0759a86cd678e3f9f75745f2eba3be452a7f3
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-03-02 19:57:49 +0200

    Fixed bugs in Type.java

--- ex3/src/TYPES/TYPE.java
+++ ex3/src/TYPES/TYPE.java
@@ -25,18 +25,24 @@ public abstract class TYPE
 	 * @return True if every instance_type object is an instance of the specified type or inherits from it; otherwise, false.
 	 */
 	public static boolean areMatchingTypes(TYPE instance_type, TYPE super_type) {
+
+		
 		if (instance_type instanceof TYPE_CLASS) {
 			TYPE_CLASS temp = (TYPE_CLASS) instance_type;
-
 			// Traverse the class hierarchy using the father reference
 			while (temp != null) {
-				if (super_type == instance_type) {
+				if (super_type == temp) {
 					return true;
 				}
 				temp = temp.father;
 			}
 			// If the super_type is not found in the class hierarchy, return false
 			return false;
+		}
+		
+		// nil is an instane of every class type.
+		if (instance_type == TYPE_NIL.getInstance() && super_type instanceof TYPE_CLASS) {
+			return true;
 		}
 
 		// If the instance_type is not a class type, compare it directly with the super_type
