f28ce3ccd1efc2b5948e8eb6df1dc0e4c800cc5b
AlonPolsky <alon.polski@mail.tau.ac.il>
2024-03-07 12:01:08 +0200

    Done.

--- ex3/src/TYPES/TYPE.java
+++ ex3/src/TYPES/TYPE.java
@@ -23,6 +23,14 @@ public abstract class TYPE
 	 * @param instance_type The type of the object to be checked.
 	 * @param super_type    The type to check against as a potential superclass.
 	 * @return true if every instance_type object is an instance of the specified type or inherits from it; otherwise, false.
+	 * 
+	 * @note Type matching for arries is defined by both types being the same,
+	 * which means we need to be cautious for AST nodes representing non-terminals that appear on the left-hand side of a derivation
+	 * rule where's the right hand sides contains an newExp, and that's because for example for the next code:
+	 * 		array idk = int[];
+	 * 		idk lol := new int[1];
+	 * simply checking that areMatchingTypes(typeOf(idk lol), typeOf(new int[1])) won't suffice (There's no way to extract from "new int[1]"" that that's a match.).
+	 * Before changing this design decision please talk to me, alot of thaught has been put to this - Alon.
 	 */
 	public static boolean areMatchingTypes(TYPE instance_type, TYPE super_type) {
 		if (instance_type instanceof TYPE_CLASS) {
@@ -41,11 +49,6 @@ public abstract class TYPE
 		// nil is an instance of every class and array types, and only of those.
 		if (instance_type == TYPE_NIL.getInstance()) {
 			return ((super_type instanceof TYPE_CLASS) || (super_type instanceof TYPE_ARRAY));
-		}
-
-		// If instance_type is an array stype, than super must be an array_type and both data types should match.
-		if (instance_type instanceof TYPE_ARRAY) {
-			return (super_type instanceof TYPE_ARRAY) && areMatchingTypes(((TYPE_ARRAY)instance_type).dataType, ((TYPE_ARRAY)super_type).dataType);
 		}
 
 		// If the instance_type is not a class type, compare it directly with the super_type
